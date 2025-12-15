4589ae0439fd3281714ab4d71bf3da594fa756f3
winstonwumbo <wjw5326@psu.edu>
2025-12-15 14:34:53 -0500

    Fixed vagrant-bootstrap for submodule

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -3,15 +3,11 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y git curl build-essential binwalk
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --no-confirm
-# git clone https://github.com/mathiashro/nixwrt.git
-
-# git clone -n  https://github.com/nixos/nixpkgs.git && \
-#    (cd nixpkgs && git checkout bc675971dae581ec653fa6)
 
 mkdir -p ~/.config/nixpkgs/
 echo "{ allowUnsupportedSystem = true; }" > ~/.config/nixpkgs/config.nix
 
-cp -r /vagrant ~/nixwrehost
+git clone --recurse-submodules git@github.com:winstonwumbo/cse-597-nixWRehosT.git
 cd nixwrehost
 
 curl https://fastly-cdn.system-rescue.org/releases/12.02/systemrescue-12.02-amd64.iso -O
